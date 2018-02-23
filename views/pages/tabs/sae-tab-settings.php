8ab766b9b6176d3614d88d17fc5fb3dc9d1417b8
Augustas <augustas@sender.lt>
2018-02-23 09:34:48 +0200

    text domain switch

--- views/pages/tabs/sae-tab-settings.php
+++ views/pages/tabs/sae-tab-settings.php
@@ -3,28 +3,28 @@
         exit; // Exit if accessed directly
     }
 // Only if woo is found
-  if(get_option('sender_woocommerce_has_woocommerce')) {
-    $allowImport = get_option('sender_woocommerce_allow_import');
-    $allowTrack = get_option('sender_woocommerce_allow_guest_track');
-    $allowSubscribe = get_option('sender_woocommerce_registration_track');
-    $customersList = get_option('sender_woocommerce_customers_list');
-    $swNewUsersList = get_option('sender_woocommerce_registration_list');
+  if(get_option('sender_automated_emails_has_woocommerce')) {
+    $allowImport = get_option('sender_automated_emails_allow_import');
+    $allowTrack = get_option('sender_automated_emails_allow_guest_track');
+    $allowSubscribe = get_option('sender_automated_emails_registration_track');
+    $customersList = get_option('sender_automated_emails_customers_list');
+    $saeNewUsersList = get_option('sender_automated_emails_registration_list');
     $lists = $sender_api->getAllLists();
-    $allowHighAcc = get_option('sender_woocommerce_high_acc');
+    $allowHighAcc = get_option('sender_automated_emails_high_acc');
   }
 ?>
 <h1>Settings</h1>
 
 <div class="content">
     
-    <div class="pure-g sender-woocommerce-section">
+    <div class="pure-g sender-net-automated-emails-section">
 
         <div class="pure-u-1-1">
             <h3><i class="zmdi zmdi-account"></i> Plugin is connected to Sender.net</h3>
         </div>
 
-        <div class="pure-u-1-1 pure-u-sm-3-24 sw-details-settings">
-            <a style="width: 80%; background-color:red" id="tye" href="options-general.php?page=sender-woocommerce&action=disconnect" class="sender-woocommerce-button">Disconnect</a>
+        <div class="pure-u-1-1 pure-u-sm-3-24 sae-details-settings">
+            <a style="width: 80%; background-color:red" id="tye" href="options-general.php?page=sender-net-automated-emails&action=disconnect" class="sender-net-automated-emails-button">Disconnect</a>
             
         </div>
 
@@ -48,44 +48,44 @@
     
  
     <?php if(isset($lists[0]->id)): ?>
-    <div class="pure-g sender-woocommerce-section">
+    <div class="pure-g sender-net-automated-emails-section">
 
         <div class="pure-u-1-1">
-            <h3 style="margin-bottom:0px;"><i class="zmdi zmdi-accounts-list-alt"></i> Auto subscribe new users is <?php echo !$allowSubscribe ? '<span id="swSubscribeStatus" style="color:red;">disabled</span>' : '<span id="swSubscribeStatus" style="color:green;">enabled</span>'; ?></h3>
+            <h3 style="margin-bottom:0px;"><i class="zmdi zmdi-accounts-list-alt"></i> Auto subscribe new users is <?php echo !$allowSubscribe ? '<span id="saeSubscribeStatus" style="color:red;">disabled</span>' : '<span id="saeSubscribeStatus" style="color:green;">enabled</span>'; ?></h3>
             <h3 style="display: inline-block"><i class="zmdi zmdi-account"></i> Save new users emails to: </h3>
             <?php if(isset($lists[0]->id)): ?>
-            <select id="swNewUsersList" style="margin-bottom: 10px;">
+            <select id="saeNewUsersList" style="margin-bottom: 10px;">
                 <option value="#" disabled selected>Select list</option>
                 <?php
                     
                     foreach($lists as $list) {
-                        echo '<option value="' . $list->id . '" ' . ($list->id == $swNewUsersList['id'] ? 'selected' : '') . '>' . $list->title . '</option>';
+                        echo '<option value="' . $list->id . '" ' . ($list->id == $saeNewUsersList['id'] ? 'selected' : '') . '>' . $list->title . '</option>';
                     }
                 ?>
             </select>
             <?php else: ?>
-            <?php update_option('sender_woocommerce_registration_list', array('id' => false, 'title' => '')); ?>
+            <?php update_option('sender_automated_emails_registration_list', array('id' => false, 'title' => '')); ?>
             <?php echo $sender_helper->showNotice('Please create a list in order to track new users', 'error'); ?>
             <?php endif; ?>
         </div>
         
         <script>
-            jQuery('#swNewUsersList').on('change', function (event) {
+            jQuery('#saeNewUsersList').on('change', function (event) {
                 var data = {
-                    listId: jQuery('#swNewUsersList option:selected').val(),
-                    title: jQuery('#swNewUsersList option:selected').text(),
+                    listId: jQuery('#saeNewUsersList option:selected').val(),
+                    title: jQuery('#saeNewUsersList option:selected').text(),
                     action: 'change_register_list'
                 }
-                jQuery('#swNewUsersList').attr('disabled', true);
+                jQuery('#saeNewUsersList').attr('disabled', true);
                 jQuery.post( "<?php echo get_admin_url();?>admin-ajax.php", data, function(response) {
-                      jQuery('#swNewUsersList').removeAttr('disabled');
+                      jQuery('#saeNewUsersList').removeAttr('disabled');
                 });
 
             });
         </script>
 
-        <div class="pure-u-1-1 pure-u-sm-3-24 sw-details-settings">
-            <button id="swSubscribeButton" style="width: 90%; background-color:<?php echo !$allowSubscribe ? 'green' : 'red'; ?>" class="sender-woocommerce-button"><?php echo !$allowSubscribe ? 'Enable' : 'Disable'; ?></button>
+        <div class="pure-u-1-1 pure-u-sm-3-24 sae-details-settings">
+            <button id="saeSubscribeButton" style="width: 90%; background-color:<?php echo !$allowSubscribe ? 'green' : 'red'; ?>" class="sender-net-automated-emails-button"><?php echo !$allowSubscribe ? 'Enable' : 'Disable'; ?></button>
         </div>
 
 
@@ -100,16 +100,16 @@
 
     </div>
     <?php else: ?>
-    <?php update_option('sender_woocommerce_registration_track', 0); ?>
+    <?php update_option('sender_automated_emails_registration_track', 0); ?>
     <?php endif; ?>
     
     
-    <?php if(get_option('sender_woocommerce_has_woocommerce')): ?>
+    <?php if(get_option('sender_automated_emails_has_woocommerce')): ?>
         <?php if(isset($lists[0]->id)): ?>
-        <div class="pure-g sender-woocommerce-section">
+        <div class="pure-g sender-net-automated-emails-section">
 
             <div class="pure-u-1-1">
-                <h3 style="margin-bottom:0px;"><i class="zmdi zmdi-accounts-list-alt"></i> Guest cart tracking is <?php echo !$allowTrack ? '<span id="swTrackStatus" style="color:red;">disabled</span>' : '<span id="swTrackStatus" style="color:green;">enabled</span>'; ?></h3>
+                <h3 style="margin-bottom:0px;"><i class="zmdi zmdi-accounts-list-alt"></i> Guest cart tracking is <?php echo !$allowTrack ? '<span id="saeTrackStatus" style="color:red;">disabled</span>' : '<span id="saeTrackStatus" style="color:green;">enabled</span>'; ?></h3>
                 <h3 style="display: inline-block;"><i class="zmdi zmdi-email"></i> Save guest customers emails to:</h3>
                 <?php  if(isset($lists[0]->id)): ?>
                 <select id="customersList" style="margin-bottom: 10px;">
@@ -122,7 +122,7 @@
                     ?>
                 </select>
                 <?php else: ?>
-                <?php update_option('sender_woocommerce_customers_list', array('id' => false, 'title' => '')); ?>
+                <?php update_option('sender_automated_emails_customers_list', array('id' => false, 'title' => '')); ?>
                 <?php echo $sender_helper->showNotice('Please create a list in order to track guest customers', 'error'); ?>
                 <?php endif; ?>
             </div>
@@ -141,8 +141,8 @@
                 });
             </script>
 
-            <div class="pure-u-1-1 pure-u-sm-3-24 sw-details-settings">
-                <button id="swCartTrackButton" style="width: 90%; background-color:<?php echo !$allowTrack ? 'green' : 'red'; ?>" class="sender-woocommerce-button"><?php echo !$allowTrack ? 'Enable' : 'Disable'; ?></button>
+            <div class="pure-u-1-1 pure-u-sm-3-24 sae-details-settings">
+                <button id="saeCartTrackButton" style="width: 90%; background-color:<?php echo !$allowTrack ? 'green' : 'red'; ?>" class="sender-net-automated-emails-button"><?php echo !$allowTrack ? 'Enable' : 'Disable'; ?></button>
             </div>
 
 
@@ -157,19 +157,19 @@
             
         </div>
         <?php else: ?>
-        <?php update_option('sender_woocommerce_allow_guest_track', 0); ?>
+        <?php update_option('sender_automated_emails_allow_guest_track', 0); ?>
         <?php endif; ?>
     
     
-    <div class="pure-g sender-woocommerce-section">
+    <div class="pure-g sender-net-automated-emails-section">
         
         <div class="pure-u-1-1">
-            <h3><i class="zmdi zmdi-wallpaper"></i> Product block import is <?php echo !$allowImport ? '<span id="swAllowImportTitle" style="color:red;">disabled</span>' : '<span id="swAllowImportTitle" style="color:green;">enabled</span>'; ?> </h3>  
+            <h3><i class="zmdi zmdi-wallpaper"></i> Product block import is <?php echo !$allowImport ? '<span id="saeAllowImportTitle" style="color:red;">disabled</span>' : '<span id="saeAllowImportTitle" style="color:green;">enabled</span>'; ?> </h3>  
         </div>
         
-        <div class="pure-u-1-1 pure-u-sm-3-24 sw-details-settings">
+        <div class="pure-u-1-1 pure-u-sm-3-24 sae-details-settings">
 
-        <button id="swAllowImportButton" style="width: 90%; background-color:<?php echo !$allowImport ? 'green' : 'red'; ?>" class="sender-woocommerce-button"><?php echo !$allowImport ? 'Enable' : 'Disable'; ?></button>
+        <button id="saeAllowImportButton" style="width: 90%; background-color:<?php echo !$allowImport ? 'green' : 'red'; ?>" class="sender-net-automated-emails-button"><?php echo !$allowImport ? 'Enable' : 'Disable'; ?></button>
         </div>
 
         <div class="pure-u-1-1 pure-u-sm-12-24">
@@ -183,15 +183,15 @@
         
     </div>
     
-    <div class="pure-g sender-woocommerce-section">
+    <div class="pure-g sender-net-automated-emails-section">
         
         <div class="pure-u-1-1">
-            <h3><i class="zmdi zmdi-alert-circle-o"></i> High accuracy mode is <?php echo !$allowHighAcc ? '<span id="swAllowHighAccTitle" style="color:red;">disabled</span>' : '<span id="swAllowHighAccTitle" style="color:green;">enabled</span>'; ?> </h3>  
+            <h3><i class="zmdi zmdi-alert-circle-o"></i> High accuracy mode is <?php echo !$allowHighAcc ? '<span id="saeAllowHighAccTitle" style="color:red;">disabled</span>' : '<span id="saeAllowHighAccTitle" style="color:green;">enabled</span>'; ?> </h3>  
         </div>
         
-        <div class="pure-u-1-1 pure-u-sm-3-24 sw-details-settings">
+        <div class="pure-u-1-1 pure-u-sm-3-24 sae-details-settings">
 
-        <button id="swAllowHighAccButton" style="width: 90%; background-color:<?php echo !$allowHighAcc ? 'green' : 'red'; ?>" class="sender-woocommerce-button"><?php echo !$allowHighAcc ? 'Enable' : 'Disable'; ?></button>
+        <button id="saeAllowHighAccButton" style="width: 90%; background-color:<?php echo !$allowHighAcc ? 'green' : 'red'; ?>" class="sender-net-automated-emails-button"><?php echo !$allowHighAcc ? 'Enable' : 'Disable'; ?></button>
         </div>
 
         <div class="pure-u-1-1 pure-u-sm-12-24">

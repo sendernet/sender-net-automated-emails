c8fae9328d71af34e4a083a089a6eb27f52f2862
Augustas <augustas@sender.lt>
2018-02-23 09:38:23 +0200

    text domain switch

--- uninstall.php
+++ uninstall.php
@@ -13,26 +13,26 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
 global $wpdb;
 
 // Delete carts table
-$carts_table = $wpdb->prefix . "sender_woocommerce_carts";
+$carts_table = $wpdb->prefix . "sender_automated_emails_carts";
 $carts_delete = "DROP TABLE " . $carts_table ;
 $wpdb->query( $carts_delete );
 
 /// Delete users table
-$users_table = $wpdb->prefix . "sender_woocommerce_users";
+$users_table = $wpdb->prefix . "sender_automated_emails_users";
 $users_delete = "DROP TABLE " . $users_table ;
 $wpdb->query( $users_delete );
 
 // Delete options
-delete_option( 'sender_woocommerce_api_key' );
-delete_option( 'sender_woocommerce_plugin_active' );
-delete_option( 'sender_woocommerce_allow_guest_track' );
-delete_option( 'sender_woocommerce_allow_import' );
-delete_option( 'sender_woocommerce_allow_forms' );
-delete_option( 'sender_woocommerce_customers_list' );
-delete_option( 'sender_woocommerce_has_woocommerce' );
-delete_option( 'sender_woocommerce_high_acc' );
-delete_option( 'sender_woocommerce_registration_list' );
-delete_option( 'sender_woocommerce_registration_track' );
-delete_option( 'sender_woocommerce_allow_push' );
-delete_option( 'sender_woocommerce_cart_period' );
-delete_option( 'sender_woocommerce_forms_list' );
+delete_option( 'sender_automated_emails_api_key' );
+delete_option( 'sender_automated_emails_plugin_active' );
+delete_option( 'sender_automated_emails_allow_guest_track' );
+delete_option( 'sender_automated_emails_allow_import' );
+delete_option( 'sender_automated_emails_allow_forms' );
+delete_option( 'sender_automated_emails_customers_list' );
+delete_option( 'sender_automated_emails_has_woocommerce' );
+delete_option( 'sender_automated_emails_high_acc' );
+delete_option( 'sender_automated_emails_registration_list' );
+delete_option( 'sender_automated_emails_registration_track' );
+delete_option( 'sender_automated_emails_allow_push' );
+delete_option( 'sender_automated_emails_cart_period' );
+delete_option( 'sender_automated_emails_forms_list' );
